3f3bbdad0b838e3718f9c8de576b18219ecde127
mtaddei <mtaddei@uw.edu>
2020-03-14 15:00:41 -0700

    added the iterated test for the watershed function

---
+++ test_watershed.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2
+
+from watershed import *
+
+def test_watershed():
+	image= 'example_images/cut_images/zoom/sem_2_cut_zoom.jpg'
+	result = watershed(image)
+	assert result.size == 8400, "Wrong size of the contours matrix"
+	assert result.shape == (80, 105), "Wrong shape of the countours matrix"
+
+#The end 
