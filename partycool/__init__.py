10ee134cc904bc2552ea9101eeac0cbf6eed2503
XiminHu <xhu66@uw.edu>
2020-03-15 13:06:40 -0700

    function update

--- partycool/__init__.py
+++ partycool/__init__.py
@@ -1,19 +1,17 @@
-#Basic packages
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-#Image modules
 import cv2
+import skimage
+import math
+from skimage import io
+import matplotlib.pyplot as plt
 from scipy.ndimage import gaussian_filter
+from skimage.morphology import reconstruction
 from scipy import stats
 from statistics import mean 
 from collections import OrderedDict
 import plotly.graph_objects as go
+import pandas as pd
 
-#Corner detection module
-import skimage
-from skimage import io
-from skimage.morphology import reconstruction
+#Optional modules
 from skimage.feature import corner_harris, corner_subpix, corner_peaks
 from skimage.transform import warp, AffineTransform
