e005d5348efb2994543edf07cdf059518d14e5ca
XiminHu <xhu66@uw.edu>
2020-03-15 12:28:03 -0700

    path fix

--- partycool/tests/test_watershed.py
+++ partycool/tests/test_watershed.py
@@ -4,7 +4,7 @@ import cv2
 from watershed import *
 
 def test_watershed():
-	image= 'example_images/cut_images/zoom/sem_2_cut_zoom.jpg'
+	image= '/Users/margheritataddei/Desktop/DIRECT/PROJECT/partycool/example_images/cut_images/zoom/sem_2_cut_zoom.jpg'
 	result = watershed(image)
 	assert result.size == 8400, "Wrong size of the contours matrix"
 	assert result.shape == (80, 105), "Wrong shape of the countours matrix"
