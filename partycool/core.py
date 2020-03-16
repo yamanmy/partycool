ed203c0cc011a3929951d99c4f3c58f5eaa9020f
yangwshi <yangwshi@uw.edu>
2020-03-16 10:27:43 -0700

    add summary and plot function

--- partycool/core.py
+++ partycool/core.py
@@ -219,6 +219,199 @@ def shape_radar(contours, img, thresh_di = 1.09, thres_poly = 1.75):
 
     return dilated_c
 
+def partycool_summary(contours, convert_factor = 0, enable_poly = True, thresh_di = 1.09, thres_poly = 1.75, spherical= False ):
+    '''
+    returns a dataframe that summarized the particle information
+    '''
+    
+    if enable_poly == True:
+        contours = contours
+    elif enable_poly == False:
+        avg_c = peri_avg(contours)
+        contours = [contour for contour in contours if (cv2.arcLength(contour, True) / avg_c) <= thresh_di]
+    
+    
+    
+    loc_x = []
+    loc_y = []
+    aspect_r = []
+    area_l = []
+    arealen_r = []
+    category_l = []
+    cX_au=[]
+    cY_au=[]
+    height = []
+    width = []
+    com_arr = np.empty((0, 2))
+    angles = []
+    
+    for c in contours:
+        x,y,w,h = cv2.boundingRect(c)
+        aspect_ratio = float(w)/h
+        area = cv2.contourArea(c)
+        avg_c = peri_avg(contours)
+        len_c = round(cv2.arcLength(c, True), 1)
+        r_peri = len_c / avg_c
+        # center of mass of each particle
+        M = cv2.moments(c)
+        if M["m00"] != 0:
+            cX = int(M["m10"] / M["m00"])
+            cY= int(M["m01"] / M["m00"])
+        else:
+          # set values as what you need in the situation
+            cX, cY = 1,1  
+        
+        #particle arientation
+             
+        try:
+            (com), _, angle = cv2.fitEllipse(c)
+        except:
+            continue
+        com = np.array(com)
+        com_arr = np.append(com_arr, [com], axis=0)
+        angles.append(int(angle))
+    
+        
+        category = ''
+
+        if r_peri <= thresh_di:
+            category = 'monomer'
+        elif r_peri > thresh_di and r_peri <= thres_poly:
+            if area > 900:
+                category = 'dimer'
+            else:
+                category = 'monomer'
+        elif r_peri > thres_poly:
+            category = 'polymer'
+
+        loc_x.append(x)
+        loc_y.append(y)
+        aspect_r.append(float('%.2g' % aspect_ratio))
+        area_l.append(int(area))
+        category_l.append(category)
+        cX_au.append(cX)
+        cY_au.append(cY)
+        height.append(w)
+        width.append(h)
+    
+    #aspact ratio correction
+    aspect_ratio = []
+    for i in range(len(aspect_r)):
+        if aspect_r[i] < 1:
+            aspect_ratio.append(1/aspect_r[i])
+        else:
+            aspect_ratio.append(aspect_r[i])
+    #Radius calculation
+    #Conversion part
+    if convert_factor != 0:
+        area_list = [int(a * (convert_factor ** 2)) for a in area_l]
+        height_list = [int(a*convert_factor) for a in width]
+        width_list = [int(a*convert_factor) for a in height]
+        #Radius calculation
+        r = [int(math.sqrt(a/math.pi)) for a in area_list]
+    elif convert_factor == 0:
+        area_list = [int(a) for a in area_l]
+        height_list = [int(a) for a in width]
+        width_list = [int(a) for a in height]
+        r = [int(math.sqrt(a/math.pi)) for a in area_list]
+        
+    if spherical==True:
+        result_dict = {'Cx' : cX_au, 
+                       'Cy' : cY_au,
+                       'Area (nm\u00b2)' : area_list,
+                       'Radius (nm)' : r,
+                       'Category' : category_l}
+    elif spherical==False:
+          result_dict = {'Cx' : cX_au, 
+                         'Cy' : cY_au,
+                         'Height (nm)' : width_list,
+                         'Width (nm)' : height_list,
+                         'Aspect Ratio' : aspect_ratio,
+                         'Area (nm\u00b2)' : area_list,
+                         'Angles (°)' :angles,
+                         'Category' : category_l}
+                     #change the 'Area (nm\u00b2)' to area_list        
+                
+    result_df = pd.DataFrame.from_dict(result_dict)
+    
+    return result_df
+
+def partycool_plots(dataframe, interactive = False):
+    '''
+    dataframe: df from partycool_summary
+    aspect: what user want to see
+    interactive: define whether the plots be interactive
+    '''
+    if interactive == False:
+        plt.rc('xtick', labelsize = 16)
+        plt.rc('xtick.major', pad = 15)
+        plt.rc('ytick', labelsize = 20)
+        plt.rc('lines', lw = 2.2, markersize = 7.5)
+        plt.rc('legend', fontsize = 16)
+        plt.rc('axes', linewidth=2.2)
+
+        fig, ax = plt.subplots(2, 2, figsize=(16, 12))
+
+        ax[0][0].hist(df['Angles (°)'], bins=40, color='purple',label = 'Area')
+        ax[0][0].set_xlabel('Angles (°)', fontsize = 18)
+        ax[0][0].set_ylabel('Count', fontsize = 18)
+        ax[0][0].grid(True)
+
+        ax[0][1].hist(df['Aspect Ratio'], bins=40, color='purple',label = 'Area')
+        ax[0][1].set_xlabel('Aspect Ratio', fontsize = 18)
+        ax[0][1].set_ylabel('Count', fontsize = 18)
+        ax[0][1].grid(True)
+
+        ax[1][0].hist(df['Height (nm)'], bins=40, color='purple',label = 'Area')
+        ax[1][0].set_xlabel('Length of height (nm)', fontsize = 18)
+        ax[1][0].set_ylabel('Count', fontsize = 18)
+        ax[1][0].grid(True)
+
+        ax[1][1].hist(df['Width (nm)'], bins=40, color='purple',label = 'Area')
+        ax[1][1].set_xlabel('Length of width (nm)', fontsize = 18)
+        ax[1][1].set_ylabel('Count', fontsize = 18)
+        ax[1][1].grid(True)
+        
+    elif interactive == True:
+        fig = make_subplots(rows=2, cols=2)
+        subplot_titles = ['Area', 'Aspect Ratio', 'Height', 'Width']
+        xaxis_title=['Angles','Aspect Ratio','Height (nm)', 'Width (nm)']
+        yaxis_title='count'
+        rows = 2
+        cols = 2
+        trace = [go.Histogram(x=df['Angles (°)'], marker_color='#800080',opacity=0.75),\
+                 go.Histogram( x=df['Aspect Ratio'],opacity=0.75),\
+                go.Histogram(x=df['Height (nm)'],opacity=0.75),go.Histogram(x=df['Width (nm)'],opacity=0.75)]
+
+        for i, col in enumerate(subplot_titles):
+            r = int(np.ceil(((i+1)/cols)))
+            c = i%2+1
+            fig.add_trace(trace[i], row=r, col=c)
+            fig.update_xaxes(title_text=xaxis_title[i], row=r, col=c)
+            fig.update_yaxes(title_text=yaxis_title, row=r, col=c)
+            fig.update_layout(bargap=0.15,showlegend=False) 
+        fig.show()
+    
+    #pie chart
+    count_mono = 0
+    count_di = 0
+    count_poly = 0
+    for i in range(len(df['Category'])):
+        if df['Category'][i] =='monomer':
+            count_mono +=1
+        elif df['Category'][i] =='dimer':
+            count_di += 1
+        elif df['Category'][i] =='polymer':
+            count_poly += 1
+    colors = ['gold', 'darkorange', 'lightgreen']
+    fig = go.Figure(data=[go.Pie(labels=['Monomer','Dimer','Polymer'],
+                                 values=[count_mono, count_di, count_poly],title='Categories')])
+    fig.update_traces(hoverinfo='label+percent', textinfo='value', textfont_size=14,
+                      marker=dict(colors=colors, line=dict(color='#000000', width=2)))
+    fig.show()
+    
+    return
+
 
 # Watershed for distinguish shapes -- beta 
 def watershed(image):
